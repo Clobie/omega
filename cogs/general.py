88aabf1d1f6d328cffd874e2becc6db4c4001350
Clobie <calebtalexander@gmail.com>
2024-11-28 01:25:11 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -13,7 +13,7 @@ class General(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         logger.info(f'Logged in as {self.bot.user}')
-    
+
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
@@ -25,39 +25,47 @@ class General(commands.Cog):
             await ctx.send(f'An error occurred: {str(error)}')
             logger.error(f'An error occurred: {str(error)}')
             raise error
-    
+
     @commands.Cog.listener()
     async def on_command(self, ctx):
-        server_name = ctx.guild.name.encode('unicode_escape').decode('utf-8')
-        channel_name = ctx.channel.name.encode('unicode_escape').decode('utf-8')
+        server_name = getattr(ctx.guild, 'name', 'DM')
+        channel_name = getattr(ctx.channel, 'name', 'Direct Message')
         command_content = ctx.message.content.encode('unicode_escape').decode('utf-8')
         channel_id = ctx.channel.id
-        logger.info(f"Command '{command_content}' entered by {ctx.author} in {server_name} ({channel_name}) [channel id: {channel_id}]")
+        
+        user_info = f"{ctx.author.name} (ID: {ctx.author.id})" if ctx.guild else f"{ctx.author.name} (ID: {ctx.author.id})"
+        logger.info(f"Command '{command_content}' entered by {user_info} in {server_name} ({channel_name}) [channel id: {channel_id}]")
 
     @commands.Cog.listener()
     async def on_command_completion(self, ctx):
-        server_name = ctx.guild.name.encode('unicode_escape').decode('utf-8')
-        channel_name = ctx.channel.name.encode('unicode_escape').decode('utf-8')
+        server_name = getattr(ctx.guild, 'name', 'DM')
+        channel_name = getattr(ctx.channel, 'name', 'Direct Message')
         command_content = ctx.message.content.encode('unicode_escape').decode('utf-8')
         channel_id = ctx.channel.id
-        logger.info(f"Command '{command_content}' completed by {ctx.author} in {server_name} ({channel_name}) [channel id: {channel_id}]")
+
+        user_info = f"{ctx.author.name} (ID: {ctx.author.id})"
+        logger.info(f"Command '{command_content}' completed by {user_info} in {server_name} ({channel_name}) [channel id: {channel_id}]")
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        server_name = message.guild.name.encode('unicode_escape').decode('utf-8')
-        channel_name = message.channel.name.encode('unicode_escape').decode('utf-8')
+        server_name = getattr(message.guild, 'name', 'DM')
+        channel_name = getattr(message.channel, 'name', 'Direct Message')
         command_content = message.content.encode('unicode_escape').decode('utf-8')
         channel_id = message.channel.id
-        logger.info(f"Message from {message.author} in {server_name} ({channel_name}) [channel id: {channel_id}]: {command_content}")
+
+        user_info = f"{message.author.name} (ID: {message.author.id})"
+        logger.info(f"Message from {user_info} in {server_name} ({channel_name}) [channel id: {channel_id}]: {command_content}")
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        server_name = before.guild.name.encode('unicode_escape').decode('utf-8')
-        channel_name = before.channel.name.encode('unicode_escape').decode('utf-8')
+        server_name = getattr(before.guild, 'name', 'DM')
+        channel_name = getattr(before.channel, 'name', 'Direct Message')
         before_content = before.content.encode('unicode_escape').decode('utf-8')
         after_content = after.content.encode('unicode_escape').decode('utf-8')
         channel_id = before.channel.id
-        logger.info(f"Message edited by {before.author} in {server_name} ({channel_name}) [channel id: {channel_id}]:"
+
+        user_info = f"{before.author.name} (ID: {before.author.id})"
+        logger.info(f"Message edited by {user_info} in {server_name} ({channel_name}) [channel id: {channel_id}]:"
             f"\nBefore: {before_content}"
             f"\nAfter: {after_content}")
 
