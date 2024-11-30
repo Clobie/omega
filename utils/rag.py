9233ad6dedb2f2b1fc7433c503c26e44b9ad5d8b
Clobie <calebtalexander@gmail.com>
2024-11-30 14:35:19 -0500

    rag placeholder

---
+++ utils/rag.py
@@ -0,0 +1,20 @@
+# cogs/rag.py
+
+import os
+from discord.ext import commands
+from utils.config import cfg
+from utils.log import logger
+from utils.status import status
+from utils.common import common
+
+class Rag():
+    def __init__(self):
+        pass
+
+    def store_document(self, document):
+        pass
+    
+    def search_documents(self):
+        pass
+
+rag = Rag()
