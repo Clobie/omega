044222fe7fb0759c846fd13d93748cb8e8f3dcdd
Clobie <calebtalexander@gmail.com>
2024-12-04 19:12:15 -0500

    make giphy a util

--- cogs/reactor.py
+++ cogs/reactor.py
@@ -7,35 +7,18 @@ from utils.ai import ai
 from utils.common import common
 from utils.config import cfg
 from utils.log import logger
+from utils.giphy import gfy
 
 class Reactor(commands.Cog):
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-        self.respond_chance = 10
+        self.respond_chance = 15
 
     async def react_gif(self, message):
         prompt = message.content.replace(str(f"<@{self.bot.user.id}>"), "").strip()
-        search_string = ai.chat_completion(
-            'gpt-4o-mini', 
-            'Analyze the text and suggest a concise search string for finding a relevant GIF. Your search string should be short and relevant.',
-            prompt
-        )
-        url = "https://api.giphy.com/v1/gifs/search"
-        params = {
-            "api_key": cfg.GIPHY_API_KEY,
-            "q": search_string,
-            "limit": 25,
-            "offset": random.randint(0, 24),
-            "rating": "r",
-            "lang": "en",
-            "bundle": "messaging_non_clips"
-        }
-        response = requests.get(url, params=params)
-        data = response.json()
-        if data['data']:
-            react_gif_url = data['data'][0]['url']
-            await message.channel.send(react_gif_url)
+        react_gif_url = await gfy.get_react_gif_url(prompt)
+        await message.channel.send(react_gif_url)
     
     @commands.Cog.listener()
     async def on_message(self, message):
---
+++ utils/giphy.py
@@ -0,0 +1,36 @@
+# cogs/reactor.py
+
+import random
+import requests
+from utils.ai import ai
+from utils.common import common
+from utils.config import cfg
+from utils.log import logger
+
+class Giphy():
+    def __init__(self):
+        self.respond_chance = 10
+        self.api_url = "https://api.giphy.com/v1/gifs/search"
+
+    async def get_react_gif_url(self, message):
+        search_string = ai.chat_completion(
+            'gpt-4o-mini',
+            'Analyze the text and suggest a concise search string for finding a relevant GIF. Your search string should be short and relevant.',
+            message
+        )
+        params = {
+            "api_key": cfg.GIPHY_API_KEY,
+            "q": search_string,
+            "limit": 25,
+            "offset": random.randint(0, 24),
+            "rating": "r",
+            "lang": "en",
+            "bundle": "messaging_non_clips"
+        }
+        response = requests.get(self.api_url, params=params)
+        data = response.json()
+        if data['data']:
+            react_gif_url = data['data'][0]['url']
+            return react_gif_url
+
+gfy = Giphy()
