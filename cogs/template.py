f4cc00bfcf75052215e017aa9ab4de2d1328693b
Clobie <calebtalexander@gmail.com>
2024-12-06 18:12:04 -0500

    Update template.py

--- cogs/template.py
+++ cogs/template.py
@@ -1,10 +1,15 @@
 # cogs/template.py
 
+import os
+import discord
 from discord.ext import commands
 from utils.config import cfg
 from utils.log import logger
 from utils.status import status
 from utils.common import common
+from utils.database import db
+from utils.credit import credit
+from utils.role import role
 
 class TemplateCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
