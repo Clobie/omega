3cfbc2ab552eb958d452e5eae611ef0f67004b7e
Clobie <calebtalexander@gmail.com>
2024-12-06 18:46:52 -0500

    template cleanup

--- cogs/template.py
+++ cogs/template.py
@@ -3,12 +3,7 @@
 import os
 import discord
 from discord.ext import commands
-from utils.config import cfg
-from utils.log import logger
-from utils.status import status
-from utils.common import common
-from utils.credit import credit
-from utils.role import role
+from core.omega import omega
 
 class TemplateCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
