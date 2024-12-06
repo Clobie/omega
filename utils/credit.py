091811a124a27d7767f91b2d1a24e61fd3510ca9
Clobie <calebtalexander@gmail.com>
2024-12-06 18:11:58 -0500

    credit util placeholder

---
+++ utils/credit.py
@@ -0,0 +1,15 @@
+# utils/credit.py
+
+import os
+import discord
+from utils.config import cfg
+from utils.log import logger
+from utils.status import status
+from utils.common import common
+from utils.database import db
+
+class Credit:
+    def __init__(self):
+        pass
+
+credit = Credit()
