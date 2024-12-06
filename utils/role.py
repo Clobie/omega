e77b6663be08ba791e554fab589c1f2789474696
Clobie <calebtalexander@gmail.com>
2024-12-06 18:11:52 -0500

    role util placeholder

---
+++ utils/role.py
@@ -0,0 +1,16 @@
+# utils/credit.py
+
+import os
+import discord
+from utils.config import cfg
+from utils.log import logger
+from utils.status import status
+from utils.common import common
+from utils.database import db
+from utils.credit import credit
+
+class Role:
+    def __init__(self):
+        pass
+
+role = Role()
