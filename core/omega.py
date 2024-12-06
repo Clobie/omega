cfc316ab066f055dfce4ec0a2b5841820ec98fb1
Clobie <calebtalexander@gmail.com>
2024-12-06 18:46:37 -0500

    add utils to core

--- core/omega.py
+++ core/omega.py
@@ -5,10 +5,23 @@ from discord.ext import commands
 from utils.cog import cog
 from utils.config import cfg
 from utils.log import logger
+from utils.status import status
+from utils.common import common
+from utils.credit import credit
+from utils.role import role
+from utils.database import db
 
 class Omega:
     def __init__(self):
         self.bot = commands.Bot(command_prefix=cfg.COMMAND_PREFIX, intents=discord.Intents.all())
+        self.cog = cog
+        self.cfg = cfg
+        self.logger = logger
+        self.status = status
+        self.common = common
+        self.credit = credit
+        self.role = role
+        self.db = db
 
     async def run(self):
         logger.info('Starting bot...')
@@ -24,3 +37,5 @@ class Omega:
         async with self.bot:
             await cog.load_cogs(self.bot)
             await self.bot.start(cfg.DISCORD_BOT_TOKEN)
+
+omega = Omega()
