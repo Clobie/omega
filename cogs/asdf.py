e5dd09be6be087f8c5fd000d06a84557dae85d0c
Clobie <calebtalexander@gmail.com>
2024-12-07 15:29:52 -0500

    Update asdf.py

--- cogs/asdf.py
+++ cogs/asdf.py
@@ -13,8 +13,9 @@ class Asdf(commands.Cog):
     async def on_ready(self):
         for guild in self.bot.guilds:
             for member in guild.members:
-                omega.db.run_script(('INSERT INTO discord_users (id, name, credits, message_count) VALUES (1, "User1", 100, 50)')(member.id, member.name, 100, 0))
-                omega.logger.info(f'Inserted user into discord_users table - Name: {member.name}, ID: {member.id}')
+                query = f'INSERT INTO discord_users (id, name, credits, message_count) VALUES ({member.id}, {member.name}, 0, 0)'
+                omega.db.run_script(query)
+                omega.logger.info(query)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Asdf(bot))
