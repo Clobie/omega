99f207a35fbdc5babd139667c3ba4abca12c31e8
Clobie <calebtalexander@gmail.com>
2024-12-07 18:01:29 -0500

    move internal credit functions to the util

--- cogs/credits.py
+++ cogs/credits.py
@@ -12,98 +12,16 @@ class Credits(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         pass
-    
-    def init_user(self, user_id):
-        query_user = (
-            "INSERT INTO discord_users (user_id, credits) "
-            "VALUES (%s, 0) "
-            "ON CONFLICT (user_id) DO NOTHING"
-        )
-        formatted_user_query = query_user % user_id
-        omega.logger.info(formatted_user_query)
-        omega.db.run_script(formatted_user_query)
-
-    def get_credits(self, user_id):
-        query = (
-            "SELECT credits FROM discord_users WHERE user_id = %s;"
-        )
-        formatted_query = query % (user_id)
-        result = omega.db.run_script(formatted_query)
-        if not result:
-            self.init_user(user_id)
-        return result[0][0] if result else 0
-    
-    def give_credits(self, user_from, user_to, amount):
-        if amount <= 0:
-            return False
-        from_credits = self.get_credits(user_from)
-        to_credits = self.get_credits(user_to)
-
-        if from_credits is None or to_credits is None:
-            return False
-        
-        if from_credits < amount:
-            return False
-
-        query = (
-            "WITH deducted AS ("
-            "    UPDATE discord_users "
-            "    SET credits = credits - %s "
-            "    WHERE user_id = %s "
-            "    RETURNING credits "
-            "), "
-            "added AS ("
-            "    UPDATE discord_users "
-            "    SET credits = credits + %s "
-            "    WHERE user_id = %s "
-            "    RETURNING credits "
-            ") "
-            "SELECT (SELECT credits FROM deducted) AS from_credits, "
-            "       (SELECT credits FROM added) AS to_credits;"
-        )
-        formatted_query = query % ((amount, user_from, amount, user_to))
-        omega.db.run_script(formatted_query)
-        return True
-    
-    def gift_credits(self, user_to, amount):
-        if amount <= 0:
-            return False
-        to_credits = self.get_credits(user_to)
-        if to_credits is None:
-            return False
-        query = (
-            "UPDATE discord_users "
-            "SET credits = credits + %s "
-            "WHERE user_id = %s "
-            "RETURNING credits;"
-        )
-        formatted_query = query % (amount, user_to)
-        new_credits = omega.db.run_script(formatted_query)
-        return new_credits is not None
-    
-    def take_credits(self, user_from, amount):
-        if amount <= 0:
-            return False
-        from_credits = self.get_credits(user_from)
-        if from_credits is None or from_credits < amount:
-            return False
-        query = (
-            "UPDATE discord_users "
-            "SET credits = credits - %s "
-            "WHERE user_id = %s "
-            "RETURNING credits;"
-        )
-        formatted_query = query % (amount, user_from)
-        new_credits = omega.db.run_script(formatted_query)
-        return new_credits is not None
 
     @commands.command(name='credits')
-    async def credits(self, ctx):
+    async def credits(self, ctx, user: discord.User = None):
         """
-        See how many credits you have
+        See how many credits you have or another user has
         """
-        credits = self.get_credits(ctx.author.id)
-        await ctx.send(f"You have {credits} credits.")
+        user_id = user.id if user else ctx.author.id
+        credits = omega.credit.get_credits(user_id)
+        user_name = user.name if user else ctx.author.name
+        await ctx.send(f"{user_name} has {credits} credits.")
 
     @commands.command(name='give')
     async def give(self, ctx, amount: int, member: discord.Member):
@@ -115,7 +33,7 @@ class Credits(commands.Cog):
             return
         user_from_id = ctx.author.id
         user_to_id = member.id
-        if self.give_credits(user_from_id, user_to_id, amount):
+        if omega.credit.give_credits(user_from_id, user_to_id, amount):
             await ctx.send(f"You've given {amount} credits to {member.mention}.")
         else:
             await ctx.send("You don't have enough credits.")
@@ -128,7 +46,7 @@ class Credits(commands.Cog):
         if amount <= 0:
             await ctx.send("Specify a valid amount of credits to give.")
             return
-        if self.gift_credits(member.id, amount):
+        if omega.credit.gift_credits(member.id, amount):
             await ctx.send(f"You've given {amount} credits to {member.mention}.")
     
     @commands.command(name='take')
@@ -139,7 +57,7 @@ class Credits(commands.Cog):
         if amount <= 0:
             await ctx.send("Specify a valid amount of credits to take.")
             return
-        if self.take_credits(member.id, amount):
+        if omega.credit.take_credits(member.id, amount):
             await ctx.send(f"You've taken {amount} credits from {member.mention}.")
         else:
             await ctx.send(f"Failed to take credits from {member.mention}. They may not have enough credits.")
--- utils/credit.py
+++ utils/credit.py
@@ -6,9 +6,94 @@ from utils.config import cfg
 from utils.log import logger
 from utils.status import status
 from utils.common import common
+from utils.database import db
 
 class Credit:
     def __init__(self):
         pass
+
+    def init_user(self, user_id):
+        query_user = (
+            "INSERT INTO discord_users (user_id, credits) "
+            "VALUES (%s, 0) "
+            "ON CONFLICT (user_id) DO NOTHING"
+        )
+        formatted_user_query = query_user % user_id
+        logger.info(formatted_user_query)
+        db.run_script(formatted_user_query)
+
+    def get_credits(self, user_id):
+        query = (
+            "SELECT credits FROM discord_users WHERE user_id = %s;"
+        )
+        formatted_query = query % (user_id)
+        result = db.run_script(formatted_query)
+        if not result:
+            self.init_user(user_id)
+        return result[0][0] if result else 0
+    
+    def give_credits(self, user_from, user_to, amount):
+        if amount <= 0:
+            return False
+        from_credits = self.get_credits(user_from)
+        to_credits = self.get_credits(user_to)
+
+        if from_credits is None or to_credits is None:
+            return False
+        
+        if from_credits < amount:
+            return False
+
+        query = (
+            "WITH deducted AS ("
+            "    UPDATE discord_users "
+            "    SET credits = credits - %s "
+            "    WHERE user_id = %s "
+            "    RETURNING credits "
+            "), "
+            "added AS ("
+            "    UPDATE discord_users "
+            "    SET credits = credits + %s "
+            "    WHERE user_id = %s "
+            "    RETURNING credits "
+            ") "
+            "SELECT (SELECT credits FROM deducted) AS from_credits, "
+            "       (SELECT credits FROM added) AS to_credits;"
+        )
+        formatted_query = query % ((amount, user_from, amount, user_to))
+        db.run_script(formatted_query)
+        return True
+    
+    def gift_credits(self, user_to, amount):
+        if amount <= 0:
+            return False
+        to_credits = self.get_credits(user_to)
+        if to_credits is None:
+            return False
+        query = (
+            "UPDATE discord_users "
+            "SET credits = credits + %s "
+            "WHERE user_id = %s "
+            "RETURNING credits;"
+        )
+        formatted_query = query % (amount, user_to)
+        new_credits = db.run_script(formatted_query)
+        return new_credits is not None
+    
+    def take_credits(self, user_from, amount):
+        if amount <= 0:
+            return False
+        from_credits = self.get_credits(user_from)
+        if from_credits is None or from_credits < amount:
+            return False
+        query = (
+            "UPDATE discord_users "
+            "SET credits = credits - %s "
+            "WHERE user_id = %s "
+            "RETURNING credits;"
+        )
+        formatted_query = query % (amount, user_from)
+        new_credits = db.run_script(formatted_query)
+        return new_credits is not None
 
 credit = Credit()
