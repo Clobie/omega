e003ef530587b1d5ea25e9223842d59b67c3d39b
Clobie <calebtalexander@gmail.com>
2024-12-09 19:59:41 -0500

    Update assistant.py

--- cogs/assistant.py
+++ cogs/assistant.py
@@ -119,12 +119,13 @@ class Assistant(commands.Cog):
                 file = discord.File('file.txt')
                 await ctx.send(attachments=[file])
                 omega.logger.debug("Response message exceeded 4000 characters, sent as a file.")
-            elif len(response_with_footer) > 2000:
-                embed = discord.Embed(description=response_with_footer)
+            #elif len(response_with_footer) > 2000:
+            else:
+                embed = omega.embed.create_embed("", response_with_footer)
                 await ctx.send(embed=embed)
                 omega.logger.debug("Response message exceeded 2000 characters, sent as an embed.")
-            else:
-                await ctx.send(content=response_with_footer)
+            #else:
+            #    await ctx.send(content=response_with_footer)
 
 
     @commands.Cog.listener()
