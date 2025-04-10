e5021e8d7c34f6e8d226595d3e5661b4c37a8419
Clobie <calebtalexander@gmail.com>
2025-04-10 10:48:55 -0400

    Update cleanup.py

--- cogs/cleanup.py
+++ cogs/cleanup.py
@@ -27,38 +27,38 @@ class Cleanup(commands.Cog):
         if ctx.message.mentions:
             user = ctx.message.mentions[0]
             deleted = await ctx.channel.purge(limit=None, check=lambda m: m.author == user)
-            await ctx.send(f"Deleted {len(deleted)} messages from {user.mention}.", delete_after=5)
+            await ctx.send(f"Deleted {len(deleted)} messages from {user.mention}.\n*This message will delete in 5 seconds.*", delete_after=5)
             return
         
         if val == 'bots':
             deleted = await ctx.channel.purge(limit=None, check=lambda m: m.author.bot)
-            await ctx.send(f"Deleted {len(deleted)} messages from bots.", delete_after=5)
+            await ctx.send(f"Deleted {len(deleted)} messages from bots.\n*This message will delete in 5 seconds.*", delete_after=5)
             return
 
         if val == 'all':
-            await ctx.channel.purge(limit=None)
-            await ctx.send("Deleted all messages.", delete_after=5)
+            deleted = await ctx.channel.purge(limit=None)
+            await ctx.send(f"Deleted {len(deleted)} messages.\n*This message will delete in 5 seconds.*", delete_after=5)
             return
 
         if val == 'images':
             await ctx.channel.purge(limit=None, check=lambda m: m.attachments)
-            await ctx.send("Deleted all images.", delete_after=5)
+            await ctx.send("Deleted all images.\n*This message will delete in 5 seconds.*", delete_after=5)
             return
 
         if val == 'today':
             await ctx.channel.purge(limit=None, check=lambda m: m.created_at.date() == datetime.datetime.utcnow().date())
-            await ctx.send("Deleted all messages from today.", delete_after=5)
+            await ctx.send("Deleted all messages from today.\n*This message will delete in 5 seconds.*", delete_after=5)
             return
 
         if val.isdigit():
             deleted = await ctx.channel.purge(limit=int(val))
-            await ctx.send(f"Deleted {len(deleted)} messages.", delete_after=5)
+            await ctx.send(f"Deleted {len(deleted)} messages.\n*This message will delete in 5 seconds.*", delete_after=5)
 
         date = self.parse_flexible_date(val)
         if date:
             try:
                 await ctx.channel.purge(limit=None, check=lambda m: m.created_at.date() == date)
-                await ctx.send(f"Deleted all messages from {date}.", delete_after=5)
+                await ctx.send(f"Deleted all messages from {date}.\n*This message will delete in 5 seconds.*", delete_after=5)
             except Exception as e:
                 await ctx.send(f"An error occurred while purging messages: {e}", delete_after=5)
         else:
