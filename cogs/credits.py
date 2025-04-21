eeb4f9c4f9273fbd6e9d8f936c99f9c2f3e287e8
Clobie <calebtalexander@gmail.com>
2025-04-20 20:45:59 -0400

    Update credits.py

--- cogs/credits.py
+++ cogs/credits.py
@@ -38,10 +38,15 @@ class Credits(commands.Cog):
         await ctx.send(f"{user_name} has {credits} credits.")
 
     @commands.command(name='give')
-    async def give(self, ctx, amount: int, member: discord.Member):
+    async def give(self, ctx, member: discord.Member, amount: int):
         """
         Give credits to another user
         """
+
+        if not isinstance(member, discord.Member) or not isinstance(amount, int):
+            await ctx.send("Command usage is !give @user amount")
+            return
+
         if amount <= 0:
             await ctx.send("Specify a valid amount of credits to give.")
             return
@@ -53,7 +58,10 @@ class Credits(commands.Cog):
             await ctx.send("You don't have enough credits.")
     
     @commands.command(name='gift')
-    async def gift(self, ctx, amount: int, member: discord.Member):
+    async def gift(self, ctx, member: discord.Member, amount: int):
+        if not isinstance(member, discord.Member) or not isinstance(amount, int):
+            await ctx.send("Command usage is !gift @user amount")
+            return
         if not ctx.author.id == int(omega.cfg.BOT_OWNER):
             await ctx.send("You do not have the required permissions for that command.")
             return
@@ -64,17 +72,20 @@ class Credits(commands.Cog):
             await ctx.send(f"You've given {amount} credits to {member.mention}.")
     
     @commands.command(name='take')
-    async def take(self, ctx, amount: int, user: discord.Member):
+    async def take(self, ctx, member: discord.Member, amount: int):
+        if not isinstance(member, discord.Member) or not isinstance(amount, int):
+            await ctx.send("Command usage is !take @user amount")
+            return
         if not ctx.author.id == int(omega.cfg.BOT_OWNER):
             await ctx.send("You do not have the required permissions for that command.")
             return
         if amount <= 0:
             await ctx.send("Specify a valid amount of credits to take.")
             return
-        if omega.credit.take_user_credits(user.id, amount):
-            await ctx.send(f"You've taken {amount} credits from {user.mention}.")
+        if omega.credit.take_user_credits(member.id, amount):
+            await ctx.send(f"You've taken {amount} credits from {member.mention}.")
         else:
-            await ctx.send(f"Failed to take credits from {user.mention}. They may not have enough credits.")
+            await ctx.send(f"Failed to take credits from {member.mention}. They may not have enough credits.")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Credits(bot))
